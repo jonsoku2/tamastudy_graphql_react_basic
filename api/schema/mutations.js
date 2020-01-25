e7cdf2eb5a5027bfedf27bc06d11b2742dee0513
rakutenjongseok <whdtjr2792@gmail.com>
2020-01-26 05:42:07 +0900

    [api] mutation으로 addPost 기능 구현

--- api/schema/mutations.js
+++ api/schema/mutations.js
@@ -1,1 +1,22 @@
-// mutations
+const graphql = require('graphql');
+const { GraphQLObjectType, GraphQLString } = graphql;
+const Post = require('../models/Post');
+const PostType = require('./types/type_post');
+
+const mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    addPost: {
+      type: PostType,
+      args: {
+        title: { type: GraphQLString },
+        description: { type: GraphQLString },
+      },
+      resolve(parentValue, args) {
+        return new Post(args).save();
+      },
+    },
+  },
+});
+
+module.exports = mutation;
--- api/schema/schema.js
+++ api/schema/schema.js
@@ -2,7 +2,9 @@ const graphql = require('graphql');
 const { GraphQLSchema } = graphql;
 
 const RootQuery = require('./rootQuery');
+const mutations = require('./mutations');
 
 module.exports = new GraphQLSchema({
   query: RootQuery,
+  mutation: mutations,
 });
